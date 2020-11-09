ec6dbc3da3af752a0e75c25e535fe257a325d18f
Shubhra <shubhrasinghal2015@gmail.com>
2020-11-09 21:55:05 +0530

    Handling the multiple windows on internet-herokupapp website

---
+++ src/main/java/com/ajax/MultipleWindowsHerokuapp.java
@@ -0,0 +1,31 @@
+package com.ajax;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.Iterator;
+import java.util.Set;
+
+public class MultipleWindowsHerokuapp {
+    public static void main(String[] args){
+        System.setProperty("webdriver.chrome.driver","C:\\work\\chromedriver.exe");
+        WebDriver driver= new ChromeDriver();
+        driver.get("http://the-internet.herokuapp.com/windows");
+        driver.manage().window().maximize();
+        driver.findElement(By.xpath("//*[text()='Click Here']")).click();
+        Set<String> ids = driver.getWindowHandles();
+        Iterator<String> it = ids.iterator();
+        String  parentId= it.next();
+        String childId= it.next();
+
+        driver.switchTo().window(childId);
+        System.out.println(driver.getTitle());
+
+        driver.switchTo().window(parentId);
+        System.out.println(driver.getTitle());
+        driver.close();
+
+    }
+
+}
